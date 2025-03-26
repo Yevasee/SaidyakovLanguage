03045afe596d47eff88aeebc52a9e8e11011bf15
Евгений Сайдяков <esaidyakov@hotmail.com>
2025-03-26 14:30:34 +0500

    23 lab: UpdateClients + ChangePage

--- SaidyakovLanguage/PageClients.xaml.cs
+++ SaidyakovLanguage/PageClients.xaml.cs
@@ -20,13 +20,105 @@ namespace SaidyakovLanguage
     /// </summary>
     public partial class PageClients : Page
     {
+        int countRecordsOfClients;
+        int countOfPages;
+        int currentPage = 0;
+        List<Client> listClientsForPage = new List<Client>();
+        List<Client> listCurrentClients;
         public PageClients()
         {
             InitializeComponent();
 
-            var clientsItemList = SaidyakovLanguageEntities.GetContext().Client.ToList();
+            UpdateClients();
+        }
 
-            ClientListView.ItemsSource = clientsItemList;
+        private void UpdateClients()
+        {
+            var listClients = SaidyakovLanguageEntities.GetContext().Client.ToList();
+
+            listViewClients.ItemsSource = listClients;
+            listCurrentClients = listClients;
+            
+            ChangePage(0, 0);
+        }
+        private void ChangePage(int direction, int? selectedPage)
+        {
+            listClientsForPage.Clear();
+            countRecordsOfClients = listCurrentClients.Count;
+
+            countOfPages = countRecordsOfClients / 10;
+            if (countRecordsOfClients % 10 > 0)
+            {
+                countOfPages += 1;
+            }
+
+            Boolean isUpdate = false;
+
+            if (selectedPage.HasValue)
+            {
+                if (selectedPage >= 0 && selectedPage <= countOfPages)
+                {
+                    currentPage = (int)selectedPage;
+                }
+                isUpdate = true;
+            }
+            else
+            {
+                switch (direction)
+                {
+                    case 1:
+                        if (currentPage > 0)
+                        {
+                            currentPage--;
+                            isUpdate = true;
+                        }
+                        break;
+                    case 2:
+                        if (currentPage < countOfPages - 1)
+                        {
+                            currentPage++;
+                            isUpdate = true;
+                        }
+                        break;
+                }
+            }
+
+            int countRecordsOnPage = currentPage * 10 + 10 < countRecordsOfClients ? currentPage * 10 + 10 : countRecordsOfClients;
+            for (int i = currentPage * 10; i < countRecordsOnPage; i++)
+            {
+                listClientsForPage.Add(listCurrentClients[i]);
+            }
+
+            if (isUpdate)
+            {
+                listBoxPages.Items.Clear();
+
+                for (int i = 1; i <= countOfPages; i++)
+                {
+                    listBoxPages.Items.Add(i);
+                }
+
+                listBoxPages.SelectedIndex = currentPage;
+                textBlockCountRecords.Text = countRecordsOnPage.ToString();
+                textBlockCountAllRecords.Text = " из " + countRecordsOfClients.ToString();
+                listViewClients.ItemsSource = listClientsForPage;
+
+                listViewClients.Items.Refresh();
+            }
+        }
+        private void btnLeftDir_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(1, null);
+        }
+
+        private void listBoxPages_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ChangePage(0, Convert.ToInt32(listBoxPages.SelectedItem.ToString()) - 1);
+        }
+
+        private void btnRightDir_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(2, null);
         }
     }
 }
